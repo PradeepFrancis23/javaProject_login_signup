ffaa3932a639cea2a394686e608d043e2c9c8db1
Pradeep Nadar <pradeep.nadar@fivesdigital.com>
2023-03-16 18:42:16 +0530

    first_commit_test

---
+++ src/main/java/com/mycompany/mavenjavaproject1/MavenJavaproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenjavaproject1;
+
+/**
+ *
+ * @author fsd
+ */
+public class MavenJavaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+   
+}
